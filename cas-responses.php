af37f3571791e41a7d4d38de161ae3603bec1066
Jennylyn Ramos <ramos_jennylynanne@plpasig.edu.ph>
2024-11-20 07:00:16 +0800

    first commit

--- cas-responses.php
+++ cas-responses.php
@@ -3,13 +3,11 @@ session_start();
 
 // Check if the user is logged in
 if (!isset($_SESSION['uname'])) {
-    // Redirect to login page if the session variable is not set
     header("Location: roleaccount.php");
     exit;
 }
 
-$currentDepartment= $_SESSION['department']; // Get the currently logged-in username
-
+$currentDepartment = $_SESSION['department']; // Get the current department
 $showForm = isset($_SESSION['show_event_form']) && $_SESSION['show_event_form'];
 
 require 'vendor/autoload.php';
@@ -26,207 +24,146 @@ $password = "s3jzz232brki4nnv";
 $dbname = "szk9kdwhvpxy2g77";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 $sql = "SELECT name, email, event, rate, department FROM submissions WHERE department = ?";
-
-// Prepare the statement
 $stmt = $conn->prepare($sql);
-
-// Bind the parameter to the placeholder
-$stmt->bind_param("s", $currentDepartment); // "s" means string (for the department)
-
-// Execute the prepared statement
+$stmt->bind_param("s", $currentDepartment);
 $stmt->execute();
-
-// Get the result of the query
 $result = $stmt->get_result();
+
 // Handle form submission for sending certificates
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['send_certificates'])) {
     $all_sent = true;
-    
     while ($row = $result->fetch_assoc()) {
         $name = $row['name'];
         $email = $row['email'];
         $department = $row['department'];
         $event = $row['event'];
 
-         // Generate PDF for each participant
-         $date = date("l, F j, Y");
+        // Hosted image URLs
+        $bgImageURL = 'https://ces-plp-d5e378ca4d4d.herokuapp.com/images/cert-bg.png';
+        $logoImageURL = 'https://ces-plp-d5e378ca4d4d.herokuapp.com/images/logoicon.png';
 
-         // Convert an image to a Base64-encoded string
-         $imagePath = $_SERVER['DOCUMENT_ROOT'] . '/CES/images/cert-bg.png';
-         $imageType = pathinfo($imagePath, PATHINFO_EXTENSION);
-         $imageData = file_get_contents($imagePath);
-         $base64Image = 'data:image/' . $imageType . ';base64,' . base64_encode($imageData);
- 
-         // Convert logo image to a Base64-encoded string
-         $logoPath = $_SERVER['DOCUMENT_ROOT'] . '/CES/images/logoicon.png'; // Update the path as necessary
-         $logoType = pathinfo($logoPath, PATHINFO_EXTENSION);
-         $logoData = file_get_contents($logoPath);
-         $base64Logo = 'data:image/' . $logoType . ';base64,' . base64_encode($logoData);
- 
-         $html = "
-         <html>
-         <head>
- 
-         <!-- Link Google Fonts directly -->
-         <link href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;1,500&display=swap' rel='stylesheet'>
-         <link href='https://fonts.googleapis.com/css2?family=Lilita+One&display=swap' rel='stylesheet'>
- 
-             <style>
-                 body { 
-                     text-align: center;
-                     margin: 0; 
-                     padding: 0; 
-                     font-family: 'Poppins', sans-serif;
-                 }
- 
-                 .certificate img {
-                     position: absolute;
-                     margin-top: -45px;
-                     width: 109%;
-                     margin-left: -45px;
-                     object-fit: cover;
-                     z-index: -1;
-                 }
- 
-                 .subheading {
-                     font-family: 'Poppins', sans-serif;
-                     font-size: 20px;
-                     color: #666;
-                     margin: 20px 0;
-                     margin-top: 240px;
-                     margin-left: -195px;
-                     letter-spacing: 0.5px;
-                 }
- 
-                 .name { 
-                     font-family: 'Lilita One', sans-serif;
-                     font-size: 80px;
-                     font-weight: bold;
-                     color: #333;
-                     margin: 20px 0;
-                     text-decoration: underline;
-                     font-style: italic;  /* Make it italic if cursive is not working */
-                     text-transform: uppercase; /* Convert text to uppercase */
-                     margin-top: 30px;
-                 }
-                     
-                 .details {
-                     font-family: 'Poppins', sans-serif;
-                     font-size: 22px; 
-                     color: black;
-                     line-height: 1.5;
-                     margin-top: 20px;
-                 }
- 
-                 .date {
-                     font-family: 'Poppins', sans-serif;
-                     font-size: 20px; 
-                     color: #888;
-                     margin-top: 30px;
-                 }
- 
-                 .footer {
-                     font-family: 'Poppins', sans-serif;
-                     font-size: 18px;
-                     color: black;
-                     text-align: center;
-                     margin-top: 50px;
-                 }
- 
-                 .footer-content {
-                     display: flex;
-                     justify-content: center;  /* Centers items horizontally */
-                 
-                 }
- 
-                 .footer-content img {
-                     margin-left: 340px;
-                     max-width: 80px;  /* Adjust the size of the logo */
-                     height: auto;
-                     margin-top: -3px;
-                 }
- 
-                 .footer-text {
-                     font-size: 20px;
-                     margin-left: 110px;
-                     font-weight: normal;
-                 }
-             </style>
-         </head>
-         <body>
-             <div class='certificate'>
-                 <img src='$base64Image' alt='Background'>
-                 <p class='subheading'>This certificate is proudly presented to</p>
-                 <p class='name'>" . htmlspecialchars($name) . "</p>
-                 <p class='details'>Who have participated in <strong>&quot;$event&quot;</strong> hosted by <strong>$department</strong><br> on <strong>$date</strong>.</p>
-                 <div class='footer'>
-                     <div class='footer-content'>
-                         <img src='$base64Logo' alt='Community Extension Services Logo' class='footer-logo'>
-                         <p class='footer-text'>Community Extension Services</p>
-                     </div>
-                 </div>
-             </div>
-         </body>
-         </html>
-         ";
- 
-        // Generate the PDF
-         $options = new Options();
-         $options->set('defaultFont', 'Poppins');
-         $options->set('isHtml5ParserEnabled', true);
-         $options->set('isPhpEnabled', true); // Allow PHP to be used in the HTML
-         $dompdf = new Dompdf($options);
-         $dompdf->loadHtml($html);
-         $dompdf->setPaper('A4', 'landscape');
-         $dompdf->render();
- 
-         // Save the PDF to a file
-         $pdfFileName = "certificate_" . urlencode($name) . ".pdf"; // Use a unique name
-         $pdfFilePath = "certificates/$pdfFileName"; // Directory where the file is saved
-         file_put_contents($pdfFilePath, $dompdf->output());
- 
-         // Send Email with the certificate attached
-         $mail = new PHPMailer(true);
-         try {
-             $mail->isSMTP();
-             $mail->Host = 'smtp.gmail.com'; 
-             $mail->SMTPAuth = true;
-             $mail->Username = 'communityextensionservices1@gmail.com'; 
-             $mail->Password = 'ctpy rvsc tsiv fwix'; 
-             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-             $mail->Port = 587;
- 
-             $mail->setFrom('communityextensionservices1@gmail.com', 'Community Extension Services');
-             $mail->addAddress($email);
-             $mail->Subject = 'Your Certificate of Participation';
-             $mail->Body = 'Attached is your certificate of participation.';
-             $mail->addAttachment($pdfFilePath);
- 
-             $mail->send();
-         } catch (Exception $e) {
-             $all_sent = false;
-             break; // Stop if an error occurs
-         }
- 
-         // Remove the PDF file after sending
-         unlink($pdfFilePath);
-     }
- 
-     // Send a response back to the front end
-     if ($all_sent) {
-         echo 'success';
-     } else {
-         echo 'error';
-     }
-     exit;
- }
+        // Fetch and encode images as Base64
+        $bgImageData = file_get_contents($bgImageURL);
+        $base64BgImage = 'data:image/png;base64,' . base64_encode($bgImageData);
+
+        $logoImageData = file_get_contents($logoImageURL);
+        $base64LogoImage = 'data:image/png;base64,' . base64_encode($logoImageData);
+
+        // HTML for the certificate
+        $date = date("l, F j, Y");
+        $html = "
+        <html>
+        <head>
+            <link href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;1,500&display=swap' rel='stylesheet'>
+            <link href='https://fonts.googleapis.com/css2?family=Lilita+One&display=swap' rel='stylesheet'>
+            <style>
+                body {
+                    text-align: center;
+                    margin: 0;
+                    padding: 0;
+                    font-family: 'Poppins', sans-serif;
+                }
+                .certificate img {
+                    position: absolute;
+                    margin-top: -45px;
+                    width: 109%;
+                    margin-left: -45px;
+                    object-fit: cover;
+                    z-index: -1;
+                }
+                .subheading {
+                    font-size: 20px;
+                    color: #666;
+                    margin-top: 240px;
+                }
+                .name {
+                    font-family: 'Lilita One', cursive;
+                    font-size: 80px;
+                    font-weight: bold;
+                    text-decoration: underline;
+                    text-transform: uppercase;
+                    margin: 30px 0;
+                }
+                .details {
+                    font-size: 22px;
+                    line-height: 1.5;
+                    margin-top: 20px;
+                }
+                .footer {
+                    margin-top: 50px;
+                    font-size: 18px;
+                }
+                .footer-content img {
+                    max-width: 80px;
+                    height: auto;
+                    margin-top: -3px;
+                }
+            </style>
+        </head>
+        <body>
+            <div class='certificate'>
+                <img src='$base64BgImage' alt='Background'>
+                <p class='subheading'>This certificate is proudly presented to</p>
+                <p class='name'>" . htmlspecialchars($name) . "</p>
+                <p class='details'>Who have participated in <strong>&quot;$event&quot;</strong> hosted by <strong>$department</strong> on <strong>$date</strong>.</p>
+                <div class='footer'>
+                    <div class='footer-content'>
+                        <img src='$base64LogoImage' alt='Logo'>
+                        <p>Community Extension Services</p>
+                    </div>
+                </div>
+            </div>
+        </body>
+        </html>
+        ";
+
+        // Generate PDF
+        $options = new Options();
+        $options->set('defaultFont', 'Poppins');
+        $dompdf = new Dompdf($options);
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'landscape');
+        $dompdf->render();
+
+        $pdfFilePath = "certificates/certificate_" . urlencode($name) . ".pdf";
+        file_put_contents($pdfFilePath, $dompdf->output());
+
+        // Send Email
+        $mail = new PHPMailer(true);
+        try {
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'communityextensionservices1@gmail.com';
+            $mail->Password = 'ctpy rvsc tsiv fwix';
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->Port = 587;
+
+            $mail->setFrom('communityextensionservices1@gmail.com', 'Community Extension Services');
+            $mail->addAddress($email);
+            $mail->Subject = 'Your Certificate of Participation';
+            $mail->Body = 'Attached is your certificate of participation.';
+            $mail->addAttachment($pdfFilePath);
+
+            $mail->send();
+        } catch (Exception $e) {
+            $all_sent = false;
+            break;
+        }
+
+        // Cleanup PDF file
+        unlink($pdfFilePath);
+    }
+
+    // Return response
+    echo $all_sent ? 'success' : 'error';
+    exit;
+}
 
 $sn = "l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $un = "equ6v8i5llo3uhjm";
@@ -252,10 +189,7 @@ if ($result_profile && $row_profile = $result_profile->fetch_assoc()) {
 }
 
 ?>
-<!-- HTML and the rest of the PHP code here... -->
 
-<?php
-?>
 
 <!DOCTYPE html>
 <html lang="en">
