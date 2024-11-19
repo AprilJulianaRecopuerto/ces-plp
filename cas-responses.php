248b97d5c10a86134f8f990a162004076a34a916
Jennylyn Ramos <ramos_jennylynanne@plpasig.edu.ph>
2024-11-20 06:39:32 +0800

    first commit

--- cas-responses.php
+++ cas-responses.php
@@ -159,13 +159,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['send_certificates'])) 
         </head>
         <body>
             <div class='certificate'>
-                <img src='$base64Image' alt='Background'>
+                <img src='$imagePath' alt='Background'>
                 <p class='subheading'>This certificate is proudly presented to</p>
                 <p class='name'>" . htmlspecialchars($name) . "</p>
                 <p class='details'>Who have participated in <strong>&quot;$event&quot;</strong> hosted by <strong>$department</strong><br> on <strong>$date</strong>.</p>
                 <div class='footer'>
                     <div class='footer-content'>
-                        <img src='$base64Logo' alt='Community Extension Services Logo' class='footer-logo'>
+                        <img src='$logoPath' alt='Logo'>
                         <p class='footer-text'>Community Extension Services</p>
                     </div>
                 </div>
