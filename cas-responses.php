58b231ed825c5607f91269f07beac296ce4777f8
Jennylyn Ramos <ramos_jennylynanne@plpasig.edu.ph>
2024-11-21 09:07:16 +0800

    first commit

--- cas-responses.php
+++ cas-responses.php
@@ -42,120 +42,130 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['send_certificates'])) 
         $email = $row['email'];
         $department = $row['department'];
         $event = $row['event'];
-
-        // Hosted image URLs
-        $bgImageURL = 'https://ces-plp-d5e378ca4d4d.herokuapp.com/images/cert-bg.png';
-        $logoImageURL = 'https://ces-plp-d5e378ca4d4d.herokuapp.com/images/logoicon.png';
         
         // Generate PDF for each participant
         $date = date("l, F j, Y");
-        
-        $html = "
-        <html>
-        <head>
-        
-            <!-- Link Google Fonts directly -->
-            <link href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;1,500&display=swap' rel='stylesheet'>
-            <link href='https://fonts.googleapis.com/css2?family=Lilita+One&display=swap' rel='stylesheet'>
-        
-            <style>
-                body { 
-                    text-align: center;
-                    margin: 0; 
-                    padding: 0; 
-                    font-family: 'Poppins', sans-serif;
-                }
-        
-                .certificate img {
-                    position: absolute;
-                    margin-top: -45px;
-                    width: 109%;
-                    margin-left: -45px;
-                    object-fit: cover;
-                    z-index: -1;
-                }
-        
-                .subheading {
-                    font-family: 'Poppins', sans-serif;
-                    font-size: 20px;
-                    color: #666;
-                    margin: 20px 0;
-                    margin-top: 240px;
-                    margin-left: -195px;
-                    letter-spacing: 0.5px;
-                }
-        
-                .name { 
-                    font-family: 'Lilita One', sans-serif;
-                    font-size: 80px;
-                    font-weight: bold;
-                    color: #333;
-                    margin: 20px 0;
-                    text-decoration: underline;
-                    font-style: italic;  /* Make it italic if cursive is not working */
-                    text-transform: uppercase; /* Convert text to uppercase */
-                    margin-top: 30px;
-                }
-        
-                .details {
-                    font-family: 'Poppins', sans-serif;
-                    font-size: 22px; 
-                    color: black;
-                    line-height: 1.5;
-                    margin-top: 20px;
-                }
-        
-                .date {
-                    font-family: 'Poppins', sans-serif;
-                    font-size: 20px; 
-                    color: #888;
-                    margin-top: 30px;
-                }
-        
-                .footer {
-                    font-family: 'Poppins', sans-serif;
-                    font-size: 18px;
-                    color: black;
-                    text-align: center;
-                    margin-top: 50px;
-                }
-        
-                .footer-content {
-                    display: flex;
-                    justify-content: center;  /* Centers items horizontally */
-                
-                }
-        
-                .footer-content img {
-                    margin-left: 340px;
-                    max-width: 80px;  /* Adjust the size of the logo */
-                    height: auto;
-                    margin-top: -3px;
-                }
-        
-                .footer-text {
-                    font-size: 20px;
-                    margin-left: 110px;
-                    font-weight: normal;
-                }
-            </style>
-        </head>
-        <body>
-            <div class='certificate'>
-                <img src='$bgImageURL' alt='Background'>
-                <p class='subheading'>This certificate is proudly presented to</p>
-                <p class='name'>" . htmlspecialchars($name) . "</p>
-                <p class='details'>Who have participated in <strong>&quot;$event&quot;</strong> hosted by <strong>$department</strong><br> on <strong>$date</strong>.</p>
-                <div class='footer'>
-                    <div class='footer-content'>
-                        <img src='$logoImageURL' alt='Logo'>
-                        <p class='footer-text'>Community Extension Services</p>
-                    </div>
-                </div>
-            </div>
-        </body>
-        </html>
-        ";
+
+         // Convert an image to a Base64-encoded string
+         $imagePath = $_SERVER['DOCUMENT_ROOT'] . '/CES/images/cert-bg.png';
+         $imageType = pathinfo($imagePath, PATHINFO_EXTENSION);
+         $imageData = file_get_contents($imagePath);
+         $base64Image = 'data:image/' . $imageType . ';base64,' . base64_encode($imageData);
+ 
+         // Convert logo image to a Base64-encoded string
+         $logoPath = $_SERVER['DOCUMENT_ROOT'] . '/CES/images/logoicon.png'; // Update the path as necessary
+         $logoType = pathinfo($logoPath, PATHINFO_EXTENSION);
+         $logoData = file_get_contents($logoPath);
+         $base64Logo = 'data:image/' . $logoType . ';base64,' . base64_encode($logoData);
+ 
+         $html = "
+         <html>
+         <head>
+ 
+         <!-- Link Google Fonts directly -->
+         <link href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;1,500&display=swap' rel='stylesheet'>
+         <link href='https://fonts.googleapis.com/css2?family=Lilita+One&display=swap' rel='stylesheet'>
+ 
+             <style>
+                 body { 
+                     text-align: center;
+                     margin: 0; 
+                     padding: 0; 
+                     font-family: 'Poppins', sans-serif;
+                 }
+ 
+                 .certificate img {
+                     position: absolute;
+                     margin-top: -45px;
+                     width: 109%;
+                     margin-left: -45px;
+                     object-fit: cover;
+                     z-index: -1;
+                 }
+ 
+                 .subheading {
+                     font-family: 'Poppins', sans-serif;
+                     font-size: 20px;
+                     color: #666;
+                     margin: 20px 0;
+                     margin-top: 240px;
+                     margin-left: -195px;
+                     letter-spacing: 0.5px;
+                 }
+ 
+                 .name { 
+                     font-family: 'Lilita One', sans-serif;
+                     font-size: 80px;
+                     font-weight: bold;
+                     color: #333;
+                     margin: 20px 0;
+                     text-decoration: underline;
+                     font-style: italic;  /* Make it italic if cursive is not working */
+                     text-transform: uppercase; /* Convert text to uppercase */
+                     margin-top: 30px;
+                 }
+                     
+                 .details {
+                     font-family: 'Poppins', sans-serif;
+                     font-size: 22px; 
+                     color: black;
+                     line-height: 1.5;
+                     margin-top: 20px;
+                 }
+ 
+                 .date {
+                     font-family: 'Poppins', sans-serif;
+                     font-size: 20px; 
+                     color: #888;
+                     margin-top: 30px;
+                 }
+ 
+                 .footer {
+                     font-family: 'Poppins', sans-serif;
+                     font-size: 18px;
+                     color: black;
+                     text-align: center;
+                     margin-top: 50px;
+                 }
+ 
+                 .footer-content {
+                     display: flex;
+                     justify-content: center;  /* Centers items horizontally */
+                 
+                 }
+ 
+                 .footer-content img {
+                     margin-left: 340px;
+                     max-width: 80px;  /* Adjust the size of the logo */
+                     height: auto;
+                     margin-top: -3px;
+                 }
+ 
+                 .footer-text {
+                     font-size: 20px;
+                     margin-left: 110px;
+                     font-weight: normal;
+                 }
+             </style>
+         </head>
+         <body>
+             <div class='certificate'>
+                 <img src='$base64Image' alt='Background'>
+                 <p class='subheading'>This certificate is proudly presented to</p>
+                 <p class='name'>" . htmlspecialchars($name) . "</p>
+                 <p class='details'>Who have participated in <strong>&quot;$event&quot;</strong> hosted by <strong>$department</strong><br> on <strong>$date</strong>.</p>
+                 <div class='footer'>
+                     <div class='footer-content'>
+                         <img src='$base64Logo' alt='Community Extension Services Logo' class='footer-logo'>
+                         <p class='footer-text'>Community Extension Services</p>
+                     </div>
+                 </div>
+             </div>
+         </body>
+         </html>
+         ";
+
+
         try {
             // Generate the PDF
             $options = new Options();
